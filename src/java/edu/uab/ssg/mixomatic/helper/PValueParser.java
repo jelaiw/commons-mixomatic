7d746b9a3f5005753f3b187ac7c2c43aadaf77ca
jelaiw <jelaiw@users.noreply.github.com>
2008-08-20 15:35:36 +0000

    Added first pass at javadoc comments for API.

--- src/java/edu/uab/ssg/mixomatic/helper/PValueParser.java
+++ src/java/edu/uab/ssg/mixomatic/helper/PValueParser.java
@@ -4,14 +4,34 @@ import java.util.*;
 import java.io.*;
 
 /**
+ * Parser for simple, "one p-value per line" file format.
+ *
  * @author Jelai Wang
  * @version 8/12/08
  */
 public final class PValueParser {
+	/**
+	 * This interface enables the parser to report badly formatted parts of
+	 * the file, as it is being parsed, to the client programmer.
+	 */
 	public interface BadFormatHandler {
+		/**
+		 * Handle a bad p-value, including cases where the value is
+		 * "out of bounds" or the value is not a number.
+		 * @param badPValue The string that could not be parsed to a p-value.
+		 */
 		void handleBadPValue(String badPValue);
 	}
 
+	/**
+	 * Parse an input stream, in a simple, "one p-value per line"
+	 * format, and return the p-values as a double array.
+	 *
+	 * @param in The input stream, typically a FileInputStream. This stream
+	 * will be closed.
+	 * @param handler The user-supplied BadFormatHandler.
+	 * @return The p-values parsed from the input stream.
+	 */
 	public double[] parse(InputStream in, BadFormatHandler handler) throws IOException {
 		if (in == null)
 			throw new NullPointerException("in");
