60c90d2c57c4f7cd9253fe3c552e80c1efcd3631
jelaiw <jelaiw@users.noreply.github.com>
2008-09-15 16:27:34 +0000

    Updated MiniAnalysis to use the same three thresholds (0.05, 0.001, 0.00001) selected for plotting in the Gadbury et al paper. Refactored private functions for estimating EDR, TP, and TN in MiniAnalysis for improved readability and simplicity. Added N1 and N2 as input parameters (these were hard-coded as N1 = N2 = 5 previously). Updated run.xml build file.

--- src/java/edu/uab/ssg/mixomatic/helper/MiniAnalysis.java
+++ src/java/edu/uab/ssg/mixomatic/helper/MiniAnalysis.java
@@ -14,20 +14,22 @@ import java.util.*;
 
 /**
  * A command-line "mini"-analysis program for the mix-o-matic procedure.
- * This program takes an input file name, parses this file for p-values,
- * fits the mix-o-matic mixture model, outputs the estimates of the
- * model parameters, plots a custom histogram, estimates the proportions
+ *
+ * This program takes an input file name, parses the named file for p-values,
+ * fits the mix-o-matic mixture model to the p-values, outputs the estimates 
+ * of the model parameters, plots a custom histogram, estimates the proportions
  * of interest (EDR, TP, TN) by parametric bootstrap at various, selected 
- * sample sizes and thresholds of significance, and plots the "combined"
- * plot at threshold = 0.05 and the individual EDR, TP, and TN plots. 
+ * sample sizes and thresholds for significance, and plots the "combined"
+ * plot at threshold = 0.05 and the individual EDR, TP, and TN plots at
+ * the same thresholds (0.05, 0.001, 0.00001) selected in the paper. 
  *
  * @author Jelai Wang
  */
 
 public final class MiniAnalysis {
 	public static void main(String[] args) throws IOException, MixomaticException {
+		// Parse input file.
 		String inputFileName = args[0];
-
 		PValueParser parser = new PValueParser();
 		System.out.println("Reading input file: " + inputFileName);
 		double[] pvalues = parser.parse(new FileInputStream(inputFileName), new PValueParser.BadFormatHandler() {
@@ -35,6 +37,11 @@ public final class MiniAnalysis {
 				System.err.println("Skipped bad input: " + badPValue);
 			}
 		});
+
+		// Parse N1 and N2.
+		int N1 = Integer.parseInt(args[1]);
+		int N2 = Integer.parseInt(args[2]);
+		System.out.println("N1 = " + N1 + ", N2 = " + N2);
 
 		System.out.println("Estimating mixture model.");
 		MixtureModel.Estimator estimator = new BoundedOptimizer();
@@ -48,12 +55,18 @@ public final class MiniAnalysis {
 		histogram.writePNG(new FileOutputStream("histogram.png"));
 
 		System.out.println("Estimating proportions of interest by bootstrap.");
-		int N1 = 5, N2 = 5; // Turn these into user-supplied arguments??
-		List<BootstrapEstimator.Estimate> estimates = estimateProportionsOfInterestAtVariousSampleSizesAndThresholds(estimate, N1, N2);
+		// Estimate EDR, TP, and TN at threshold = 0.05.
+		List<BootstrapEstimator.Estimate> estimates = estimateProportionsOfInterestAtVariousSampleSizes(estimate, N1, N2, 0.05);
 
 		System.out.println("Creating combined plot.");
-		CombinedPlot combined = new CombinedPlot(filterByThreshold(estimates, 0.05));
+		CombinedPlot combined = new CombinedPlot(estimates);
 		combined.writePNG(new FileOutputStream("combined.png"));
+
+		System.out.println("More estimation of proportions of interest.");
+		// Estimate EDR, TP, and TN at other selected thresholds.
+		// These are the same three thresholds selected in the paper, pg 335.
+		estimates.addAll(estimateProportionsOfInterestAtVariousSampleSizes(estimate, N1, N2, 0.001));
+		estimates.addAll(estimateProportionsOfInterestAtVariousSampleSizes(estimate, N1, N2, 0.00001));
 
 		System.out.println("Creating EDR, TP, and TN plots.");
 		EDRPlot edrPlot = new EDRPlot(estimates);
@@ -66,28 +79,15 @@ public final class MiniAnalysis {
 		System.out.println("Done.");
 	}
 
-	private static List<BootstrapEstimator.Estimate> estimateProportionsOfInterestAtVariousSampleSizesAndThresholds(MixtureModel.Estimate model, int N1, int N2) {
+	private static List<BootstrapEstimator.Estimate> estimateProportionsOfInterestAtVariousSampleSizes(MixtureModel.Estimate model, int N1, int N2, double threshold) {
 		int[] n_ = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 100 };
-		double[] thresholds = new double[] { 0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001 };
 
 		List<BootstrapEstimator.Estimate> list = new ArrayList<BootstrapEstimator.Estimate>();
 		BootstrapEstimator estimator = new DefaultEstimator();
 		for (int i = 0; i < n_.length; i++) {
-			for (int j = 0; j < thresholds.length; j++) {
-				BootstrapEstimator.Estimate estimate = estimator.estimateProportions(model, N1, N2, n_[i], thresholds[j]);
-				list.add(estimate);
-			}
+			BootstrapEstimator.Estimate estimate = estimator.estimateProportions(model, N1, N2, n_[i], threshold);
+			list.add(estimate);
 		}
 		return list;
-	}
-
-	private static List<BootstrapEstimator.Estimate> filterByThreshold(List<BootstrapEstimator.Estimate> estimates, double threshold) {
-		List<BootstrapEstimator.Estimate> filtered = new ArrayList<BootstrapEstimator.Estimate>();
-		for (Iterator<BootstrapEstimator.Estimate> it = estimates.iterator(); it.hasNext(); ) {
-			BootstrapEstimator.Estimate estimate = it.next();
-			if (estimate.getSignificanceLevel() == threshold)
-				filtered.add(estimate);
-		}
-		return filtered;
 	}
 }
